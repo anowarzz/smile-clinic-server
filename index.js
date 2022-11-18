cbe83a9ca5ebabd97b34f5c237b8cca4b82b60c5
anowarzz <anowarhosen444@gmail.com>
2022-11-18 08:49:13 +0600

    appointment options loaded from mongodb

--- index.js
+++ index.js
@@ -12,20 +12,39 @@ app.use(express.json())
 
 
 
-
+// Connecting to MongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v1rp4a3.mongodb.net/?retryWrites=true&w=majority`;
-
-console.log(uri);
-
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// MongoDb Crud Operations
+
+async function run () {
+
+try{
+
+const appointmentOptionCollection = client.db('SmileClinic').collection('appointmentOptions')
+
+app.get('/appointmentOptions', async(req, res) => {
+    const query = {}
+    const options = await appointmentOptionCollection.find(query).toArray();
+    res.send(options)
+})
+
+
+}
+
+
+finally{
+
+}
+
+
+}
+
+
+run().catch(console.log)
 
 
 
