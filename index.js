2d8769bd47ef0a48bf1402788af480604a40ac12
anowarzz <anowarhosen444@gmail.com>
2022-11-19 08:36:24 +0600

    updating remaining slots

--- index.js
+++ index.js
@@ -26,11 +26,51 @@ try{
 
 const appointmentOptionCollection = client.db('SmileClinic').collection('appointmentOptions')
 
+const bookingsCollection = client.db('SmileClinic').collection('bookings')
+
+
+
+// Use Aggregate to query multiple collection and then merge data
 app.get('/appointmentOptions', async(req, res) => {
+
+    const date = req.query.date
+    console.log(date);
+    
     const query = {}
     const options = await appointmentOptionCollection.find(query).toArray();
+// Get the booking of the provided date
+    const bookingQuery = {appointmentDate: date}
+    const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+
+// Code To Be Careful
+
+    options.forEach(option => {
+        const optionBooked = alreadyBooked.filter(book => book.treatment === option.name)
+
+    const bookedSlots = optionBooked.map(book => book.slot)
+
+    const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+
+    option.slots = remainingSlots;
+       
+    })
+
     res.send(options)
 })
+
+// Sending appointment bookings to DB
+app.post('/bookings', async(req, res) => {
+    const booking = req.body;
+    const result = await bookingsCollection.insertOne(booking)
+    res.send(result)
+})
+
+
+
+
+
+
+
 
 
 }
@@ -62,3 +102,12 @@ app.listen(port, () => {
     
 })
 
+/*
+API Naming Convention 
+
+ 1. app.get('/bookings')
+ 2. app.get('/bookings/:id')
+ 3 app.post('/bookings')
+ 4. app.patch('/bookings/:id)
+ 5. app.delete('/bookings/:id)
+*/
