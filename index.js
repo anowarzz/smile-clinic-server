26af42faf0c81e56cdb335f6ccd4a3c4ef63d05a
anowarzz <anowarhosen444@gmail.com>
2022-11-25 09:54:45 +0600

    jwt verify middlware created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -18,8 +19,27 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-// MongoDb Crud Operations
 
+// Verifying Jwt token
+function verifyJWT(req, res, next){
+  console.log('token inside verify token', req.headers.authorization);
+
+  const authHeader =  req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send('Unauthorized access')
+  }
+
+  const token = authHeader.split(' ')[1];
+
+
+
+}
+
+
+
+
+
+// MongoDb Crud Operations
 async function run() {
   try {
     const appointmentOptionCollection = client
@@ -27,6 +47,8 @@ async function run() {
       .collection("appointmentOptions");
 
     const bookingsCollection = client.db("SmileClinic").collection("bookings");
+
+    const usersCollection = client.db("SmileClinic").collection("users");
 
     // Use Aggregate to query multiple collection and then merge data
     app.get("/appointmentOptions", async (req, res) => {
@@ -130,14 +152,40 @@ async function run() {
 
 
     // Loading My appointments from server
-
-    app.get('/bookings', async(req, res) => {
+    app.get('/bookings', verifyJWT, async(req, res) => {
       const email = req.query.email;
       const query = {email : email}
       const bookings = await bookingsCollection.find(query).toArray();
       res.send(bookings)
-
     })
+
+
+
+// jwt token
+app.get('/jwt', async(req, res) => {
+
+  const email = req.query.email;
+  const query = {email: email}
+  const user = await usersCollection.findOne(query);
+
+  if(user){
+  const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1d'})
+  return res.send({accessToken: token})
+  } 
+
+  res.status(403).send({accessToken: '' })
+  
+
+})
+
+
+
+// Saving User information in database
+app.post('/users', async(req, res) => {
+  const user = req.body;
+  const result = usersCollection.insertOne(user)
+  res.send(result)
+})
 
 
   } finally {
